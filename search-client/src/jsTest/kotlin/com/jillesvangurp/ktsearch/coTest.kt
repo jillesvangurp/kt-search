97dc7abf0e23237ceb6a5aadd6139e8ea2b0e3f6
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2022-05-29 09:26:18 +0200

    Use GlobalScope anyway; weird issues with opensearch and TestScope

--- search-client/src/jsTest/kotlin/com/jillesvangurp/ktsearch/coTest.kt
+++ search-client/src/jsTest/kotlin/com/jillesvangurp/ktsearch/coTest.kt
@@ -6,8 +6,7 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.test.TestScope
 import kotlin.time.Duration
 
-private val testScope = TestScope()
-actual fun coTest(timeout: Duration, block: suspend () -> Unit): dynamic = testScope.async {
+actual fun coTest(timeout: Duration, block: suspend () -> Unit): dynamic = GlobalScope.async {
         block.invoke()
 }.asPromise()
 
