a7a44f0d2bec347be7d46d4d0f79c55a5338ea8a
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2022-05-29 08:44:46 +0200

    try using globalscope

--- search-client/src/jsTest/kotlin/com/jillesvangurp/ktsearch/coTest.kt
+++ search-client/src/jsTest/kotlin/com/jillesvangurp/ktsearch/coTest.kt
@@ -1,15 +1,12 @@
-@file:OptIn(ExperimentalCoroutinesApi::class)
-
 package com.jillesvangurp.ktsearch
 
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.*
 import kotlinx.coroutines.test.TestScope
-import kotlinx.coroutines.test.runTest
 import kotlin.time.Duration
 
-private val testScope = TestScope()
-actual fun coTest(timeout: Duration, block: suspend () -> Unit): dynamic = runTest {
-    block.invoke()
-}
+//private val testScope = TestScope()
+actual fun coTest(timeout: Duration, block: suspend () -> Unit): dynamic = GlobalScope.async {
+        block.invoke()
+}.asPromise()
 
 
