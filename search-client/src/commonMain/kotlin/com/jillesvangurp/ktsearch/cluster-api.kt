40d1aab1f11b084cb76cca61baa6112b2607c1f6
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2022-06-07 21:42:58 +0200

    disable search-after test when using opensearch
    add mechanism to run tests conditionally on the variant

--- search-client/src/commonMain/kotlin/com/jillesvangurp/ktsearch/cluster-api.kt
+++ search-client/src/commonMain/kotlin/com/jillesvangurp/ktsearch/cluster-api.kt
@@ -24,10 +24,59 @@ data class ClusterHealthResponse(
     val timedOut: Boolean,
 )
 
-suspend fun SearchClient.clusterHealth(    extraParameters: Map<String,String>?=null,
+suspend fun SearchClient.clusterHealth(
+    extraParameters: Map<String, String>? = null,
 ): ClusterHealthResponse {
     return restClient.get {
         path("_cluster", "health")
         parameters(extraParameters)
     }.parse(ClusterHealthResponse.serializer(), json)
 }
+
+
+@Serializable
+data class SearchEngineVersion(
+    val distribution: String?,
+    val number: String,
+)
+
+@Serializable
+data class SearchEngineInformation(
+    val name: String,
+    @SerialName("cluster_name")
+    val clusterName: String,
+    @SerialName("cluster_uuid")
+    val clusterUUID: String,
+    val version: SearchEngineVersion,
+) {
+    val variantInfo by lazy {
+        VariantInfo(
+            variant =
+            when {
+                this.version.distribution != null -> SearchEngineVariant.OS1
+                this.version.number.startsWith("7.") -> SearchEngineVariant.ES7
+                this.version.number.startsWith("8.") -> SearchEngineVariant.ES8
+                else -> error("version not recognized")
+            },
+            versionString = version.number
+        )
+    }
+}
+
+enum class SearchEngineVariant { ES7, ES8, OS1 }
+
+data class VariantInfo(
+    val variant: SearchEngineVariant,
+    val versionString: String,
+) {
+    val majorVersion by lazy { versionString.split('.')[0].toIntOrNull() }
+    val minorVersion by lazy { versionString.split('.')[1].toIntOrNull() }
+    val patchVersion by lazy { versionString.split('.')[2].toIntOrNull() }
+}
+
+suspend fun SearchClient.searchEngineVersion(): SearchEngineInformation {
+    return restClient.get {
+
+    }.parse(SearchEngineInformation.serializer(), json)
+}
+
