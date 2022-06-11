631985faeaccbcbbb4a124bc4ae35fef7b155068
jillesvangurp <jillesvangurp@users.noreply.github.com>
2022-06-11 08:06:23 +0000

    deploy: 08b1bf2e7406b60b058f44c31f9480198bf2a2a5

---
+++ api/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":search-client:dokkaHtml/commonMain":[],":search-client:dokkaHtml/jsMain":[":search-client:dokkaHtml/commonMain"],":search-client:dokkaHtml/jvmMain":[":search-client:dokkaHtml/commonMain"]}'
