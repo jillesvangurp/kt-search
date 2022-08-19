c762fcfcc9d7f391eb6d57c06c21a44843b2877a
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2022-08-19 11:22:01 +0200

    fixes #19 - basic auth was not set at all

--- search-client/src/jsMain/kotlin/com/jillesvangurp/ktsearch/defaultHttpClient.kt
+++ search-client/src/jsMain/kotlin/com/jillesvangurp/ktsearch/defaultHttpClient.kt
@@ -2,15 +2,30 @@ package com.jillesvangurp.ktsearch
 
 import io.ktor.client.*
 import io.ktor.client.engine.js.*
+import io.ktor.client.plugins.auth.*
+import io.ktor.client.plugins.auth.providers.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.serialization.kotlinx.json.*
 
-actual fun defaultKtorHttpClient(logging: Boolean): HttpClient {
+actual fun defaultKtorHttpClient(
+    logging: Boolean,
+    user: String?,
+    password: String?
+): HttpClient {
     return HttpClient(Js) {
-        if(logging) {
+        if (logging) {
             install(Logging) {
                 level = LogLevel.ALL
+            }
+        }
+        if (!user.isNullOrBlank() && !password.isNullOrBlank()) {
+            install(Auth) {
+                basic {
+                    credentials {
+                        BasicAuthCredentials(user,password)
+                    }
+                }
             }
         }
         install(ContentNegotiation) {
--- search-client/src/jvmMain/kotlin/com/jillesvangurp/ktsearch/defaultHttpClient.kt
+++ search-client/src/jvmMain/kotlin/com/jillesvangurp/ktsearch/defaultHttpClient.kt
@@ -2,11 +2,17 @@ package com.jillesvangurp.ktsearch
 
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.auth.*
+import io.ktor.client.plugins.auth.providers.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.serialization.kotlinx.json.*
 
-actual fun defaultKtorHttpClient(logging: Boolean): HttpClient {
+actual fun defaultKtorHttpClient(
+    logging: Boolean,
+    user: String?,
+    password: String?
+): HttpClient {
     return HttpClient(CIO) {
         engine {
             maxConnectionsCount = 100
@@ -17,7 +23,16 @@ actual fun defaultKtorHttpClient(logging: Boolean): HttpClient {
                 connectAttempts = 3
             }
         }
-        if(logging) {
+        if (!user.isNullOrBlank() && !password.isNullOrBlank()) {
+            install(Auth) {
+                basic {
+                    credentials {
+                        BasicAuthCredentials(user,password)
+                    }
+                }
+            }
+        }
+        if (logging) {
             install(Logging) {
                 level = LogLevel.ALL
             }
