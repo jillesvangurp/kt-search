79065f3e54b4d24d5db60428f59abbdb526b5103
Lyudmil Delchev <lddelchev@gmail.com>
2022-11-28 17:52:29 +0200

    Make RoundRobinNodeSelector thread-safe

--- search-client/src/commonMain/kotlin/com/jillesvangurp/ktsearch/RoundRobinNodeSelector.kt
+++ search-client/src/commonMain/kotlin/com/jillesvangurp/ktsearch/RoundRobinNodeSelector.kt
@@ -1,6 +1,9 @@
 package com.jillesvangurp.ktsearch
 
+import kotlin.native.concurrent.ThreadLocal
+
 class RoundRobinNodeSelector: NodeSelector {
+    @ThreadLocal
     private var index: Int = 0
     override fun selectNode(initialNodes: Array<out Node>): Node {
         return initialNodes[index++].also {
