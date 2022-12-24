b064197ae7808d445a0bef23b68f898187a7f729
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2022-12-24 23:46:02 +0100

    documentation improvements

--- docs/src/test/kotlin/documentation/manual/search/search.kt
+++ docs/src/test/kotlin/documentation/manual/search/search.kt
@@ -23,6 +23,10 @@ val searchMd = sourceGitRepository.md {
     +"""
         Searching is of course the main reason for using Opensearch and Elasticsearch. Kt-search supports this
         with a rich Kotlin DSL. However, you can also use string literals to search.
+        
+        The advantage of using a Kotlin DSL for writing your queries is that you can rely on Kotlin's type safety
+        and also use things like refactoring, property references to fields in your model classes, functional programming,
+        etc.
                 
     """.trimIndent()
 
@@ -89,7 +93,8 @@ val searchMd = sourceGitRepository.md {
         }
 
         +"""
-            Of course normally, you'd specify some kind of query. One way is to simply pass that as a string.
+            Of course normally, you'd specify some kind of query. One valid way is to simply pass that as a string.
+            Kotlin of course has multiline strings that can be templated as well. So, this may be all you need.
         """.trimIndent()
         suspendingBlock {
             val term = "legumes"
@@ -98,7 +103,8 @@ val searchMd = sourceGitRepository.md {
                 {
                     "query": {
                         "term": {
-                            "tags": {
+                            // using property references is a good idea
+                            "tags": {                             
                               "value":"$term"
                             }
                         }
@@ -111,6 +117,8 @@ val searchMd = sourceGitRepository.md {
 
     +"""
         Note how we are using templated strings here. With some hand crafted queries, this style of querying may be very useful.
+        
+        Another advantage is that you can paste queries straight from the Kibana development console.
     """.trimIndent()
 
     section("Using the SearchDSL") {
@@ -147,34 +155,21 @@ val searchMd = sourceGitRepository.md {
             }.ids
         }
 
-        section("Supported queries") {
+        section("Picking apart the results") {
             +"""
-            Currently the following queries are supported:
-            
-            - all term level queries (term, terms, regex, etc.)
-            - all full text queries (match, match_phrase_prefix, multi-match, etc.)
-            - all compound queries (bool, boosting, dismax, etc.)
-            - nested queries
-            - some aggregation queries (`terms`)
-            
-            Adding more queries to the DSL is easy and we welcome pull requests for this.
-        """.trimIndent()
-        }
-
-        section("Parsing the results") {
-            +"""
-                Of course a key reason for querying is to get the documents you indexed back and deserialized.
+                Of course a key reason for querying is to get the documents you indexed back and 
+                deserializing those back to your model classes.
                 
                 Here is a more complex query that returns fruit with `ban` as the name prefix.
             """.trimIndent()
             suspendingBlock {
-                client.search(indexName) {
+                val resp = client.search(indexName) {
                     from = 0
                     // size is of course also a thing in Map
                     resultSize = 100
                     // more relevant if you have more than 10k hits
                     trackTotalHits = "true" // not always a boolean in the DSL
-                    // a more complex query
+                    // a more complex bool query
                     query = bool {
                         filter(
                             term(TestDoc::tags, "fruit")
@@ -183,19 +178,27 @@ val searchMd = sourceGitRepository.md {
                             matchPhrasePrefix(TestDoc::name, "ban")
                         )
                     }
-                }.parseHits<TestDoc>().map { it?.name }
+                }
+                // deserializes all the hits
+                val hits=resp.parseHits<TestDoc>().map { it.name }
+
+                println(hits.joinToString("\n"))
+
+                // you can also do something like this:
+                println(resp.total)
+                resp.hits?.hits?.forEach { hit ->
+                    val doc = hit.parseHit<TestDoc>()
+                    println("${hit.id} - ${hit.score}: ${doc.name} (${doc.price})")
+                }
             }
-
-
+            
             +"""
                 Note how we are parsing the hits back to TestDoc here. By default, the source
                 gets deserialized as a `JsonObject`. However, with `kotlinx.serialization`, you can
                 use that as the input for `decodeFromJsonElement<T>(object)` to deserialize to some custom
                 data structure. This is something we use in multiple places.
-                
-                It uses some sane defaults for parsing that you can learn more about here: ${ManualPages.GettingStarted.page.mdLink}
-                
             """.trimIndent()
+
         }
 
         section("Compound queries") {
