e55fa766af35d128afde2dda1d464c00d2814b9c
Marius Barbulescu <marius.barbulescu@gmail.com>
2023-12-26 16:43:55 +0100

    enable setting refresh_interval in createIndex

--- search-dsls/src/commonMain/kotlin/com/jillesvangurp/searchdsls/mappingdsl/IndexSettings.kt
+++ search-dsls/src/commonMain/kotlin/com/jillesvangurp/searchdsls/mappingdsl/IndexSettings.kt
@@ -67,6 +67,7 @@ class Analysis : JsonDsl() {
 class IndexSettings : JsonDsl() {
     var replicas: Int by property("index.number_of_replicas")
     var shards: Int by property("index.number_of_shards")
+    var refreshInterval: String by property("index.refresh_interval")
     var indexLifeCycleName: String by property("index.lifecycle.name")
 
     fun analysis(block: Analysis.() -> Unit) {
