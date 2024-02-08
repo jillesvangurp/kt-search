bd644978a4556fabfea1f625b5217c9992248903
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2024-02-08 15:27:20 +0100

    upgrades

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,8 @@ pluginManagement {
 
 plugins {
     id("de.fayard.refreshVersions") version "0.60.3"
+////                            # available:"0.60.4"
+////                            # available:"0.60.5"
 }
 
 refreshVersions {
