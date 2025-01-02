8b2197423113f575231179bd14b6a2f982ac1835
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2025-01-02 15:52:05 +0100

    missing import

--- search-client/src/jsMain/kotlin/com/jillesvangurp/ktsearch/defaultHttpClient.kt
+++ search-client/src/jsMain/kotlin/com/jillesvangurp/ktsearch/defaultHttpClient.kt
@@ -3,12 +3,6 @@ package com.jillesvangurp.ktsearch
 import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.js.Js
-import io.ktor.client.plugins.auth.Auth
-import io.ktor.client.plugins.auth.providers.BasicAuthCredentials
-import io.ktor.client.plugins.auth.providers.basic
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logging
-import io.ktor.http.headers
 
 actual fun defaultKtorHttpClient(
     logging: Boolean,
--- search-client/src/linuxMain/kotlin/com/jillesvangurp/ktsearch/KtorRestClient.linux.kt
+++ search-client/src/linuxMain/kotlin/com/jillesvangurp/ktsearch/KtorRestClient.linux.kt
@@ -1,6 +1,7 @@
 package com.jillesvangurp.ktsearch
 
 import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.curl.Curl
 
 actual fun defaultKtorHttpClient(
