7291199604c454d2ae9bcef8d5a22e6cb8a0a1c3
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2025-11-03 10:09:27 +0100

    document the ModelSerializationStrategy and explain why value has to be a JsonObject

--- search-client/src/commonMain/kotlin/com/jillesvangurp/ktsearch/repository/ModelSerializationStrategy.kt
+++ search-client/src/commonMain/kotlin/com/jillesvangurp/ktsearch/repository/ModelSerializationStrategy.kt
@@ -1,8 +1,31 @@
 package com.jillesvangurp.ktsearch.repository
 
+import com.jillesvangurp.serializationext.DEFAULT_JSON
 import kotlinx.serialization.json.JsonObject
 
+/**
+ * Used by [IndexRepository] to serialize/deserialize models. The default implementation uses kotlinx.serialization.
+ *
+ * Note, even if you use Jackson, the search client still requires kotlinx.serialization for parsing responses.
+ */
 interface ModelSerializationStrategy<T:Any> {
+
     fun serialize(value: T): String
+
+    /**
+     * Deserialize a kotlinx.serialization JsonObject.
+     *
+     * The [value] parameter is a JsonObject because the Elasticsearch/Opensearch response has already
+     * been parsed using kotlinx.serialization. Passing the JsonObject avoids converting back and forth
+     * between string and JSON, while still allowing frameworks like Jackson to re-parse it if needed.
+     *
+     * For this, you can use `value.serializedToString` to serialize [value] back or call
+     * `Json.encodeToString(value)` directly.
+     */
     fun deSerialize(value: JsonObject): T
 }
+
+/**
+ * Convenience function to get to the serialized String.
+ */
+val JsonObject.serializedToString get() = DEFAULT_JSON.encodeToString(this)
